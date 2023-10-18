b00b1d92c1d2453a8554dec4f3c8a5b089064fd6
Abaadir <abaadir.yasin@student.moringaschool.com>
2023-10-18 16:06:46 +0300

    completed

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+word = word.replace(/[^a-z]/g , '').toLowerCase();
+ 
+const reversedWord= word.split('').reverse().join('');
+ return word === reversedWord;
+
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
